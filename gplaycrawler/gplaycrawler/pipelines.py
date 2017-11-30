d83169502e33c2156b13ef1ce4f52b3799a7bfc0
branden <branden.a.ehrenreich@gmail.com>
2017-11-29 21:18:36 -0500

    add retries for psql query to help with startup order

--- gplaycrawler/gplaycrawler/pipelines.py
+++ gplaycrawler/gplaycrawler/pipelines.py
@@ -36,11 +36,17 @@ class GplayPipeline(object):
 ##            raise DropItem("Duplicate item found: %s" % item)
 ##        else:
 ##            self.links_seen.append(item['Link'])
-          try:
-              cur = self.conn.cursor()
-##        cur.execute("insert into apps (link, item_name, updated, author, filesize, downloads, version, compatibility, content_rating, author_link, author_link_test, genre, price, rating_value, review_number, description, iap, developer_badge, physical_address, video_url, developer_id) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (item["Link"], item["Item_name"], item["Updated"], item["Author"], item["Filesize"], item["Downloads"], item["Version"], item["Compatibility"], item["Content_rating"], item["Author_link"], item["Author_link_test"], item["Genre"], item["Price"], item["Rating_value"], item["Review_number"], item["Description"], item["IAP"], item["Developer_badge"], item["Physical_address"], item["Video_URL"], item["Developer_ID"]))
-              cur.execute("insert into apps (app_id, item_name, updated, author, filesize, downloads, version, compatibility, content_rating, author_link, genre, price, rating_value, review_number, description, iap, developer_badge, physical_address, video_url, developer_id) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (item["Link"][46:], item["Item_name"], item["Updated"], item["Author"], item["Filesize"], item["Downloads"], item["Version"], item["Compatibility"], item["Content_rating"], item["Author_link"], item["Genre"], item["Price"], item["Rating_value"], item["Review_number"], item["Description"], item["IAP"], item["Developer_badge"], item["Physical_address"], item["Video_URL"], item["Developer_ID"]))
-              self.conn.commit()
-          except:
-              self.conn.rollback()
+          tries=0
+          max_tries=3
+          while(tries<max_tries):
+            try:
+                cur = self.conn.cursor()
+  ##        cur.execute("insert into apps (link, item_name, updated, author, filesize, downloads, version, compatibility, content_rating, author_link, author_link_test, genre, price, rating_value, review_number, description, iap, developer_badge, physical_address, video_url, developer_id) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (item["Link"], item["Item_name"], item["Updated"], item["Author"], item["Filesize"], item["Downloads"], item["Version"], item["Compatibility"], item["Content_rating"], item["Author_link"], item["Author_link_test"], item["Genre"], item["Price"], item["Rating_value"], item["Review_number"], item["Description"], item["IAP"], item["Developer_badge"], item["Physical_address"], item["Video_URL"], item["Developer_ID"]))
+                cur.execute("insert into apps (app_id, item_name, updated, author, filesize, downloads, version, compatibility, content_rating, author_link, genre, price, rating_value, review_number, description, iap, developer_badge, physical_address, video_url, developer_id) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (item["Link"][46:], item["Item_name"], item["Updated"], item["Author"], item["Filesize"], item["Downloads"], item["Version"], item["Compatibility"], item["Content_rating"], item["Author_link"], item["Genre"], item["Price"], item["Rating_value"], item["Review_number"], item["Description"], item["IAP"], item["Developer_badge"], item["Physical_address"], item["Video_URL"], item["Developer_ID"]))
+                self.conn.commit()
+            except:
+                self.conn.rollback()
+                tries+=1
+                time.sleep(1)
+
         return item  
